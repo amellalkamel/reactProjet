875a536f455cb6552a63b9c86ddacdd6b81d6c95
amellal.kameleddine <amellal.kameleddine@elit.dz>
2020-12-08 09:18:23 +0100

    pull from origin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { nom: "", age: 0, email: "", password: "", showForm: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      showForm: true,
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Nom :<br></br>
+          <input
+            type="text"
+            name="nom"
+            value={this.state.nom}
+            onChange={this.handleChange}
+          />
+          <br></br>
+        </label>
+        <label>
+          Age :<br></br>
+          <input
+            type="number"
+            name="age"
+            value={this.state.age}
+            onChange={this.handleChange}
+          />
+          <br></br>
+        </label>
+        <label>
+          Email :<br></br>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <br></br>
+        </label>
+        <label>
+          Password :<br></br>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <br></br>
+        </label>
+        <button type="submit"> Enregistrer</button>
+        {this.state.showForm && (
+          <p>
+            le nom est :{this.state.nom}
+            <br></br> l'age est :{this.state.age} <br></br> email est :{" "}
+            {this.state.email} <br></br> password est : {this.state.password}{" "}
+          </p>
+        )}
+      </form>
+    );
+  }
 }
 
 export default App;
